811e1f36d99247bc7b97dac17d93bdd890dc3815
Eduardo <00092117@uca.edu.sv>
2020-03-31 16:11:51 -0600

    Initial commit

---
+++ tareaLabo2/src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.uca.capas.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.uca.capas.domain.Usuario;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/login")
+	public String enviarForm(Usuario usuario) {
+
+		return "mostrarLogin";
+	}
+	
+	@PostMapping("/validacion")
+	public String procesarForm(Usuario usuario) {
+		String form;
+		if(usuario.getUser().equals("admin") && usuario.getPassword().equals("123")) {
+			form = "MostrarMensajeV";
+		} else {
+			form = "MostrarMensajeF";
+		}
+		return form;
+	}
+	
+}
